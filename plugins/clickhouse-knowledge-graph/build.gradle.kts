b2bdee996bba8865432d19111e5ec9c2bf974f78
Wannes Kerckhove <wannes.kerckhove@ugent.be>
2024-08-09 14:33:13 +0200

    chore: removed clickhouse-kg module as a CH implementation is not needed atm

--- plugins/clickhouse-knowledge-graph/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("kvasir-commonlib")
-    id("io.quarkus")
-}
-
-dependencies {
-    implementation(project(":libs:definitions"))
-    implementation("io.smallrye.reactive:smallrye-mutiny-vertx-web-client")
-}
