d98d914545c226de11fa6a237d08c1585665c893
Wannes Kerckhove <wannes.kerckhove@ugent.be>
2024-11-28 14:05:56 +0100

    chore: added Maven build files

--- services/storage-api/src/main/kotlin/kvasir/services/api/storage/openapi/OpenApiProvider.kt
+++ services/storage-api/src/main/kotlin/kvasir/services/api/storage/openapi/OpenApiProvider.kt
@@ -1,19 +1,10 @@
 package kvasir.services.api.storage.openapi
 
 import io.quarkus.smallrye.openapi.OpenApiFilter
-import io.smallrye.openapi.api.models.OperationImpl
-import io.smallrye.openapi.api.models.PathItemImpl
-import io.smallrye.openapi.api.models.PathsImpl
-import io.smallrye.openapi.api.models.media.ContentImpl
-import io.smallrye.openapi.api.models.media.MediaTypeImpl
-import io.smallrye.openapi.api.models.media.SchemaImpl
-import io.smallrye.openapi.api.models.parameters.ParameterImpl
-import io.smallrye.openapi.api.models.parameters.RequestBodyImpl
-import io.smallrye.openapi.api.models.responses.APIResponseImpl
-import io.smallrye.openapi.api.models.responses.APIResponsesImpl
 import kvasir.definitions.openapi.ApiDocTags
 import kvasir.services.api.storage.HEADER_X_AMZ_CONTENT_SHA256
 import kvasir.services.api.storage.HEADER_X_AMZ_DATE
+import org.eclipse.microprofile.openapi.OASFactory
 import org.eclipse.microprofile.openapi.OASFilter
 import org.eclipse.microprofile.openapi.models.OpenAPI
 import org.eclipse.microprofile.openapi.models.PathItem
@@ -24,21 +15,21 @@ import org.eclipse.microprofile.openapi.models.parameters.Parameter
 class OpenApiProvider : OASFilter {
 
     private val commonParams = listOf(
-        ParameterImpl()
+        OASFactory.createParameter()
             .`in`(Parameter.In.PATH)
             .name("podId")
             .required(true)
-            .schema(SchemaImpl().type(Schema.SchemaType.STRING)),
-        ParameterImpl()
+            .schema(OASFactory.createSchema().addType(Schema.SchemaType.STRING)),
+        OASFactory.createParameter()
             .`in`(Parameter.In.PATH)
             .name("objectKey")
             .required(true)
-            .schema(SchemaImpl().type(Schema.SchemaType.STRING))
+            .schema(OASFactory.createSchema().addType(Schema.SchemaType.STRING))
     )
 
     override fun filterOpenAPI(openAPI: OpenAPI) {
         if (openAPI.paths == null) {
-            openAPI.paths = PathsImpl()
+            openAPI.paths = OASFactory.createPaths()
         }
         generateS3Operations(openAPI, "/{podId}/s3/{objectKey}", "the specified pod's S3 storage")
         //generateS3Operations(openAPI, "/{podId}/slices/{sliceId}/s3/{objectKey}", "the specified slice's S3 storage")
@@ -49,62 +40,65 @@ class OpenApiProvider : OASFilter {
         path: String,
         targetRefDescription: String
     ) {
-        openAPI.paths.addPathItem(path, PathItemImpl().apply {
-            this.setOperation(PathItem.HttpMethod.GET, OperationImpl().apply {
+        openAPI.paths.addPathItem(path, OASFactory.createPathItem().apply {
+            this.setOperation(PathItem.HttpMethod.GET, OASFactory.createOperation().apply {
                 this.summary = "Download a stored object."
                 this.description = "Get an object from $targetRefDescription."
                 this.addTag(ApiDocTags.STORAGE_API)
                 this.parameters = commonParams
-                this.responses = APIResponsesImpl().addAPIResponse("200", APIResponseImpl().apply {
-                    this.description = "The object content"
-                })
+                this.responses =
+                    OASFactory.createAPIResponses().addAPIResponse("200", OASFactory.createAPIResponse().apply {
+                        this.description = "The object content"
+                    })
             })
-            this.setOperation(PathItem.HttpMethod.PUT, OperationImpl().apply {
+            this.setOperation(PathItem.HttpMethod.PUT, OASFactory.createOperation().apply {
                 this.summary = "Upload an object."
                 this.description = "Put an object to $targetRefDescription."
                 this.addTag(ApiDocTags.STORAGE_API)
                 this.parameters = commonParams.plus(
                     listOf(
-                        ParameterImpl()
+                        OASFactory.createParameter()
                             .`in`(Parameter.In.HEADER)
                             .name(HEADER_X_AMZ_CONTENT_SHA256)
                             .description("The SHA256 hash of the object content (allows the server to verify the integrity of the content).")
                             .required(true)
-                            .schema(SchemaImpl().type(Schema.SchemaType.STRING)),
-                        ParameterImpl()
+                            .schema(OASFactory.createSchema().addType(Schema.SchemaType.STRING)),
+                        OASFactory.createParameter()
                             .`in`(Parameter.In.HEADER)
                             .name("Content-Type")
                             .description("The MIME type of the object content.")
                             .required(true)
-                            .schema(SchemaImpl().type(Schema.SchemaType.STRING)),
-                        ParameterImpl()
+                            .schema(OASFactory.createSchema().addType(Schema.SchemaType.STRING)),
+                        OASFactory.createParameter()
                             .`in`(Parameter.In.HEADER)
                             .name(HEADER_X_AMZ_DATE)
                             .description("The date and time of the request, in ISO8601 format (offers additional protection against replay attacks).")
                             .required(false)
                             .schema(
-                                SchemaImpl().type(Schema.SchemaType.STRING)
+                                OASFactory.createSchema().addType(Schema.SchemaType.STRING)
                             )
                     )
                 )
-                this.requestBody(RequestBodyImpl().apply {
+                this.requestBody(OASFactory.createRequestBody().apply {
                     this.required(true)
-                    this.content = ContentImpl().addMediaType("*/*", MediaTypeImpl().apply {
-                        this.schema = SchemaImpl().type(Schema.SchemaType.STRING)
+                    this.content = OASFactory.createContent().addMediaType("*/*", OASFactory.createMediaType().apply {
+                        this.schema = OASFactory.createSchema().addType(Schema.SchemaType.STRING)
                     })
                 })
-                this.responses = APIResponsesImpl().addAPIResponse("200", APIResponseImpl().apply {
-                    this.description = "The object was stored successfully"
-                })
+                this.responses =
+                    OASFactory.createAPIResponses().addAPIResponse("200", OASFactory.createAPIResponse().apply {
+                        this.description = "The object was stored successfully"
+                    })
             })
-            this.setOperation(PathItem.HttpMethod.DELETE, OperationImpl().apply {
+            this.setOperation(PathItem.HttpMethod.DELETE, OASFactory.createOperation().apply {
                 this.summary = "Remove a stored object."
                 this.description = "Delete an object from $targetRefDescription."
                 this.addTag(ApiDocTags.STORAGE_API)
                 this.parameters = commonParams
-                this.responses = APIResponsesImpl().addAPIResponse("204", APIResponseImpl().apply {
-                    this.description = "The object was deleted successfully"
-                })
+                this.responses =
+                    OASFactory.createAPIResponses().addAPIResponse("204", OASFactory.createAPIResponse().apply {
+                        this.description = "The object was deleted successfully"
+                    })
             })
         })
     }
