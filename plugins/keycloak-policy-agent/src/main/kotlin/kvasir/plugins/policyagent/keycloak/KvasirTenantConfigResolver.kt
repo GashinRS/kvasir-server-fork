73ba618d49298c3f06bb22959f91d2a7889596b3
Wannes Kerckhove <wannes.kerckhove@ugent.be>
2024-12-12 16:18:26 +0100

    fix: added missing quarkus auth permission config

--- plugins/keycloak-policy-agent/src/main/kotlin/kvasir/plugins/policyagent/keycloak/KvasirTenantConfigResolver.kt
+++ plugins/keycloak-policy-agent/src/main/kotlin/kvasir/plugins/policyagent/keycloak/KvasirTenantConfigResolver.kt
@@ -1,5 +1,8 @@
 package kvasir.plugins.policyagent.keycloak
 
+import io.quarkus.keycloak.pep.TenantPolicyConfigResolver
+import io.quarkus.keycloak.pep.runtime.KeycloakPolicyEnforcerTenantConfig
+import io.quarkus.logging.Log
 import io.quarkus.oidc.OidcRequestContext
 import io.quarkus.oidc.OidcTenantConfig
 import io.quarkus.oidc.TenantConfigResolver
@@ -20,23 +23,43 @@ class KvasirTenantConfigResolver(
         routingContext: RoutingContext,
         requestContext: OidcRequestContext<OidcTenantConfig>
     ): Uni<OidcTenantConfig>? {
-        val pathItems = routingContext.request().path().split('/').filterNot{ it.isBlank() }
-        if (pathItems.isEmpty()) {
+        val pathItems = routingContext.request().path().split('/').filterNot { it.isBlank() }
+        if (pathItems.isEmpty() || pathItems.first() == "q") {
             return null
         }
         val podId = "${baseUri}${pathItems.first()}"
         return podStore.getById(podId)
             .onItem().ifNull().failWith { NotFoundException() }
             .onItem().ifNotNull().transformToUni { pod ->
-                pod?.getAuthConfiguration()?.let { authConfig ->
+                val result = pod?.getAuthConfiguration()?.let { authConfig ->
                     Uni.createFrom().item(OidcTenantConfig().apply {
+                        this.setTenantId(pathItems.first())
+                        this.setApplicationType(OidcTenantConfig.ApplicationType.HYBRID)
                         this.setAuthServerUrl(authConfig.serverUrl)
                         this.setClientId(authConfig.clientId)
                         this.credentials.setSecret(authConfig.clientSecret)
+                        Log.debug("Resolved tenant config for pod $podId: $this")
                     })
                 } ?: Uni.createFrom().failure(NotFoundException())
-
+                result
             }
     }
 
 }
+
+//@ApplicationScoped
+//class KvasirTenantPolicyConfigResolver(
+//    private val podStore: PodStore,
+//    @ConfigProperty(name = "kvasir.base-uri", defaultValue = "http://localhost:8080/")
+//    private val baseUri: String,
+//) : TenantPolicyConfigResolver {
+//
+//    override fun resolve(
+//        routingContext: RoutingContext,
+//        tenantConfig: OidcTenantConfig,
+//        requestContext: OidcRequestContext<KeycloakPolicyEnforcerTenantConfig>
+//    ): Uni<KeycloakPolicyEnforcerTenantConfig>? {
+//        return null
+//    }
+//
+//}
