47ac5a0fcff1194e244ae65b0e44261a2fb3fd9b
Jasper Vaneessen <jasper.vaneessen@proton.me>
2025-04-29 16:41:19 +0200

    chore(dependencies): set CH version to Altinity LTS 24.8

--- libs/clickhouse-test-utils/src/main/kotlin/kvasir/utils/test/clickhouse/ClickhouseTestResource.kt
+++ libs/clickhouse-test-utils/src/main/kotlin/kvasir/utils/test/clickhouse/ClickhouseTestResource.kt
@@ -2,19 +2,26 @@ package kvasir.utils.test.clickhouse
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager
 import org.testcontainers.clickhouse.ClickHouseContainer
+import org.testcontainers.utility.DockerImageName
 import org.testcontainers.utility.MountableFile
 
 class ClickhouseTestResource : QuarkusTestResourceLifecycleManager {
     private lateinit var clickhouse: ClickHouseContainer
 
-    private val clickhouseImage = System.getProperty("kvasir.datastore.kg.image-name", "clickhouse/clickhouse-server:24.8.11.5")
+    private val clickhouseImageName =
+        System.getProperty(
+            "kvasir.datastore.kg.image-name",
+            "altinity/clickhouse-server:24.8.14.10459.altinitystable",
+        )
 
     override fun start(): Map<String, String> {
-        clickhouse = ClickHouseContainer(clickhouseImage)
-            .withCopyFileToContainer(
-                MountableFile.forClasspathResource("kvasir.config.xml"),
-                "/etc/clickhouse-server/users.d/kvasir.config.xml"
-            )
+        val chImage = DockerImageName.parse(clickhouseImageName).asCompatibleSubstituteFor("clickhouse/clickhouse-server")
+        clickhouse =
+            ClickHouseContainer(chImage)
+                .withCopyFileToContainer(
+                    MountableFile.forClasspathResource("kvasir.config.xml"),
+                    "/etc/clickhouse-server/users.d/kvasir.config.xml",
+                )
         clickhouse.start()
         val conf = mutableMapOf<String, String>()
         conf["kvasir.kg.clickhouse.host"] = clickhouse.host
@@ -23,9 +30,8 @@ class ClickhouseTestResource : QuarkusTestResourceLifecycleManager {
         return conf
     }
 
-
-
     override fun stop() {
         clickhouse.stop()
     }
 }
+
