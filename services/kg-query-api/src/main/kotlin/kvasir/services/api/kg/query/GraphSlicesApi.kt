07818e155b18f9269ea200f50558f9552596f775
Wannes Kerckhove <wannes.kerckhove@ugent.be>
2025-08-12 13:51:46 +0200

    chore: Removed obsolete SHACL change request validation

--- services/kg-query-api/src/main/kotlin/kvasir/services/api/kg/query/GraphSlicesApi.kt
+++ services/kg-query-api/src/main/kotlin/kvasir/services/api/kg/query/GraphSlicesApi.kt
@@ -1,6 +1,7 @@
 package kvasir.services.api.kg.query
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import graphql.language.ObjectTypeDefinition
 import graphql.parser.Parser
 import idlab.quarkus.ext.pep.openfga.runtime.annotations.OpenFgaPolicyEnforcer
 import io.smallrye.mutiny.Multi
@@ -14,6 +15,7 @@ import jakarta.ws.rs.sse.OutboundSseEvent
 import jakarta.ws.rs.sse.Sse
 import kvasir.definitions.annotations.GenerateNoArgConstructor
 import kvasir.definitions.kg.*
+import kvasir.definitions.kg.graphql.TYPE_MUTATION
 import kvasir.definitions.kg.slices.Slice
 import kvasir.definitions.kg.slices.SliceStore
 import kvasir.definitions.kg.slices.SliceSummary
@@ -29,7 +31,6 @@ import kvasir.utils.graphql.SchemaValidator
 import kvasir.utils.http.KvasirUriInfo
 import kvasir.utils.http.getChildUri
 import kvasir.utils.http.getParentUri
-import kvasir.utils.shacl.GraphQL2SHACL
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter
@@ -367,9 +368,10 @@ class GraphSlicesApi(
 
     private fun validateAndPersistSlice(podId: String, sliceId: String, input: SliceInput): Uni<Slice> {
         return try {
-            // Generate shapes
-            val shaclConvertor = GraphQL2SHACL(input.schema, input.context)
-            val slice = input.toSlice(podId, sliceId, shaclConvertor.hasMutations())
+            // Check if the schema contains mutations
+            val doc = Parser.parse(input.schema)
+            val hasMutations = doc.definitions.filterIsInstance<ObjectTypeDefinition>().any { it.name == TYPE_MUTATION }
+            val slice = input.toSlice(podId, sliceId, hasMutations)
             // Validate the schema
             SchemaValidator.validateSchema(slice.schema, slice.context)
             sliceStore.persist(slice).map { slice }
